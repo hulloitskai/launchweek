cb52d41132fb5fdeda8e6a5b35669ce317940e86
kai <hulloitskai@gmail.com>
2023-11-19 01:18:09 -0800

    Fix tests

--- test/system/homepage_test.rb
+++ test/system/homepage_test.rb
@@ -6,6 +6,6 @@ require "application_system_test_case"
 class HomepageTest < ApplicationSystemTestCase
   test "renders" do
     visit(root_path)
-    assert_text("welcome to my little corner of the internet :)", wait: 1)
+    assert_text("LaunchWeek", wait: 1)
   end
 end
